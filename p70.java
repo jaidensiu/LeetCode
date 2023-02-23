eacd54dcb3b0947001b17e739e5536495ebb1a64
siujaiden <jaidensiu@gmail.com>
2023-02-22 23:20:41 -0800

    update

---
+++ p70.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int prev = 1;
+        int curr = 2;
+        for (int i = 3; i <= n; i++) {
+            int tmp = prev + curr;
+            prev = curr;
+            curr = tmp;
+        }
+        return curr;
+    }
+}
