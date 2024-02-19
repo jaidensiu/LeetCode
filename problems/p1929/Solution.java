5c50e9014687e2fec8b14807b8db85f073cd6f05
Jaiden Siu <jaidensiu@gmail.com>
2024-02-19 00:14:21 -0800

    completed p1299, p1929

---
+++ problems/p1929/Solution.java
@@ -0,0 +1,12 @@
+package problems.p1929;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i];
+            res[i + nums.length] = nums[i];
+        }
+        return res;
+    }
+}
